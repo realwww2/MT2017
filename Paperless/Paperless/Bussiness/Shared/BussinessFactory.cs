67798b65fe51c7a143883ddc7426caa50340bec3
realwww2 <realwww2@sina.com>
2017-04-01 21:32:59 +0800

    Add missed files

---
+++ Paperless/Paperless/Bussiness/Shared/BussinessFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Reflection;
+using System.Configuration;
+
+namespace Paperless.Bussiness
+{
+    public class BussinessFactory
+    {
+        public static BussinessFactory Instance = new BussinessFactory();
+
+        private IDictionary<Type, Object> _Services;
+        private IBussinessCreator _creator;
+        private BussinessFactory()
+        {
+            _Services = new Dictionary<Type,Object>();
+            string creatorTypeStr = ConfigurationManager.AppSettings["BussinessCreator"];
+            _creator = System.Activator.CreateInstance(Type.GetType(creatorTypeStr)) as IBussinessCreator;
+            _creator.CreateBussinessServices (_Services);
+            
+        }
+        public IDASession OpenDASession()
+        {
+            return _creator.OpenDASession();
+        }
+        public IUserService userService
+        {
+            get { return (IUserService)_Services[typeof(IUserService)]; }
+        }
+    }
+}
